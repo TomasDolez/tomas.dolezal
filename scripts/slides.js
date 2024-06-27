ea5b32ba795526db1d5f6ff88b9bc11451672900
TomasDolez <td2512199@gmail.com>
2024-06-27 13:38:24 +0200

    centering

--- scripts/slides.js
+++ scripts/slides.js
@@ -10,5 +10,5 @@ function carousel() {
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}
   x[myIndex-1].classList.add("active");
-  setTimeout(carousel, 27000); // Change image every 27 seconds
+  setTimeout(carousel, 7000); // Change image every 27 seconds
 }
